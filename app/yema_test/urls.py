a9ab09f1d4eba0baa7b8799e7158b081f28a4ade
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-05-21 02:07:40 -0500

    Changes admin header

--- app/yema_test/urls.py
+++ app/yema_test/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin 
+
+admin.site.site_header = "Yema Test Luis Daniel Guzmán"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
