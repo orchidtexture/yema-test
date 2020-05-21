94354a7cbbd52800c92e508e71303ccea7bb2a76
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-05-21 15:06:12 -0500

    added models to admin

--- app/appointments/admin.py
+++ app/appointments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Appointment, AppointmentRequest
+
 # Register your models here.
+
+admin.site.register(AppointmentRequest)
+admin.site.register(Appointment)
